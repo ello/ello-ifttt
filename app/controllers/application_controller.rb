7cca2e04491a98c2c32f79dbc4db4d5f5ce3e7d7
Jay Zeschin <jay.zeschin@modeset.com>
2016-03-03 16:55:58 -0700

    Wire up a user info endpoint based on JWT token data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+  rescue_from JWT::ExpiredSignature, with: :render_unauthorized
+
+  private
+
+  def bearer_token
+    request.authorization.split.last
+  end
+
+  def jwt_token
+    @jwt_token ||= begin
+                     JWT.decode bearer_token, OpenSSL::PKey::RSA.new(ENV['JWT_PUBLIC_KEY']), true, { :algorithm => 'RS512' }
+                   end
+  end
+
+  def jwt_header
+    jwt_token.last
+  end
+
+  def jwt_payload
+    jwt_token.first
+  end
+
+  def render_unauthorized
+    head :unauthorized
+  end
 end
