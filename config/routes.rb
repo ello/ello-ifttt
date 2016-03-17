5f25e14b99cd6da5506aa4728b5f2e3b1bce1dbb
Alan Peabody <gapeabody@gmail.com>
2016-03-17 12:02:41 -0400

    Add status for ifttt.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :ifttt do
     namespace :v1 do
       get 'user/info', to: 'users#show'
+      get 'status', to: 'status#show'
       namespace :triggers do
         post 'submitted_new_post', to: 'submitted_new_post#index'
         post 'loved_a_post', to: 'loved_a_post#index'
